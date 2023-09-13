6dc0a14789eee6570af3141042a2ed4d95f1b738
Harshita <harshitasaini469@gmail.com>
2023-09-13 14:30:37 +0530

    spelling mistake

--- src/components/HomeCustomers/Hero.jsx
+++ src/components/HomeCustomers/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       >
         <div className="space-y-5  w-1/3 flex flex-col justify-center items-center ml-28 ">
         <p className=" text-xl sm:text-2xl md:text-3xl lg:text-5xl text-center font-semibold">
-          Embarce the Spectacular Unlaeash your Eventful Journey
+          Embrace the Spectacular Unlaeash your Eventful Journey
           </p>
           <button className="bg-gradient-to-r from-fuchsia-500 to-pink-600 px-4 py-2 rounded-xl text-sm sm:text-base md:text-xl lg:text-3xl font-medium">
             Explore 
